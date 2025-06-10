79261c8e6219fb0555ee34edcec7cac5f81673e5
victor-franco13 <vfranco.dev@gmail.com>
2025-06-10 08:58:09 -0300

    Inicio do estudo em C

---
+++ C Language/01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //definição de variável:
+    //tipo nome;
+    int A;  //inteiros,d (inteiro)
+    char B; //caracteres,c (caracter)
+    float C;//Numero com ponto,f (real)
+    double D;//numero com ponto e o dobro de espaço,%
+
+    //print
+    //printf("Mostra o que está dentro das aspas");
+    //%d,%c,%f
+    printf("Nro: %d; nro+1: %d \n",A,A+1);
+
+    //scan
+    //scanf("tipos de entrada",variaveis);
+    //&d,&c,&f
+
+}
