9d922a10dbef8f86dca58725e6e2c32643ca783c
victor-franco13 <vfranco.dev@gmail.com>
2025-06-18 11:47:59 -0300

    Create 02.c

---
+++ C Language/02.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.1415
+
+
+int main(){
+    //variável = expressão;
+    int x =5, y;
+    y=x;
+    y= x+10;
+
+    int x, y, z;
+
+      <-      <-
+    x = y = z = 10;
+    <-   <-
+
+    int nro = 10;
+
+    const int nro = 10;
+
+    //#define
+
+    float f = PI + 1;
+    printf("PI = %f \n",PI);
+
+
+    
+    Soma:           +
+    Subtração:      -
+    Multiplicação:  *
+    Divisão:        /
+    Resto:          %
+    
+
+    
+    Incremento: x++;
+    Decremento: x--;
+    
+
+    
+    y=x++;
+    é igual a:
+    y=x;
+    x++;
+
+    y= ++x;
+    é igual a:
+    x++;
+    y=x;
+
+
+
+    atribuição simplificada:
+
+    variavel "operação=" expressão;
+    variavel = variavel "operação" expressão;
+    ex:
+    y+=z;
+    é igual a:
+    y = y + z; 
+
+
+    system("pause");
+    return 0;
+
+}
